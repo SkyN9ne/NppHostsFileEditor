365d2bd8171ac95342b35689881adb9170e792a7
SkyN9ne <82126207+SkyN9ne@users.noreply.github.com>
2022-12-28 11:11:04 +0200

    Update NppPluginDemo.cpp

--- src/NppPluginDemo.cpp
+++ src/NppPluginDemo.cpp
@@ -1,20 +1,3 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO <donho@altern.org>
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
 #include "PluginDefinition.h"
 
 extern FuncItem funcItem[nbFunc];
@@ -77,12 +60,8 @@ extern "C" __declspec(dllexport) void beNotified(SCNotification *notifyCode)
 	}
 }
 
-
-// Here you can process the Npp Messages 
-// I will make the messages accessible little by little, according to the need of plugin development.
-// Please let me know if you need to access to some messages :
 // http://sourceforge.net/forum/forum.php?forum_id=482781
-//
+
 extern "C" __declspec(dllexport) LRESULT messageProc(UINT /*Message*/, WPARAM /*wParam*/, LPARAM /*lParam*/)
 {/*
 	if (Message == WM_MOVE)
